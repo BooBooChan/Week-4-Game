76169a6e976ec08929e1d0001e6a6608f42c75da
Bridget Bourne <Bridget Bourne>
2017-05-29 13:18:40 -0400

    pseudo-coding for the game

--- javascript/game.js
+++ javascript/game.js
@@ -1,1 +1,22 @@
+//Step 1: Create variables on which all your functions operate (characters, stats).
 var characters = ["Mufasa", "Bambis-Mom", "Gothel", "Queen", "A-n-I", "Frollo", "Kerchak", "James"]; 
+var hp = 100
+var atk = [a,b,c,d,e,f,g,h]
+var catk = [a,b,c,d,e,f,g,h]
+//Step2: Create FUNCTIONs for your INTEGERs. Make sure attack stats are RANDOM.
+
+//Step 3: Assign stats to EACH character.
+
+/*Step 4: Create FUNCTIONs which subtract attack value from hp and add increasing multiple of base atk
+to current atk power after every turn. For balance, catk will need to be much higher than base atk.*/
+
+/*Step 5: Create an EVENT which causes characters to respond to the CLICK of a mouse. Then create a FUNCTION which causes
+opponent to counter attack after player clicks.*/
+//Caution: Keep in mind in which order the characters take their turn.
+
+//Step 6: Create a FUNCTION which removes character from roster when hp =< 0
+
+/*Step 7: Create a CONDITIONAL which displays the victory screen when all opponents are eliminated, 
+and one which displays the Game Over screen when player's hp =< 0*/
+
+//Step 8: 
